87ed05c92e278da030f37f5cc3ca6e8dc7c7c263
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-10-04 10:31:35 +0530

    fixes

--- cofee_shop/app/page.tsx
+++ cofee_shop/app/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import Hero from "./Components/Hero";
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+      <main className="overflow-hidden">
+      <Hero/>
+      </main>
+    
   );
 }
